4dcc64d80a31816359f6cbc3886f230cde2323df
viktory-ja <88890601+viktory-ja@users.noreply.github.com>
2021-12-03 20:39:29 +0100

    Day 2 ⭐

---
+++ day_2.py
@@ -0,0 +1,47 @@
+with open("input_day_2") as input_data:
+    data_lst = input_data.read().split("\n")
+
+data_lst_2d = [command.split() for command in data_lst]
+
+
+# for lst_element in data_lst_2d:
+#     lst_element[1] = int(lst_element[1])
+#
+# print(data_lst_2d)
+
+
+def calculation_submarine_position(lst):
+    horizontal_position = 0
+    depth = 0
+
+    for command in lst:
+        if command[0] == "forward":
+            horizontal_position += int(command[1])
+        elif command[0] == "down":
+            depth += int(command[1])
+        else:
+            depth -= int(command[1])
+    return horizontal_position * depth
+
+
+print(calculation_submarine_position(data_lst_2d))
+
+
+def calculation_planned_course(lst):
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for command in lst:
+        if command[0] == "forward":
+            horizontal_position += int(command[1])
+            depth += int(command[1]) * aim
+        elif command[0] == "down":
+            aim += int(command[1])
+        else:
+            aim -= int(command[1])
+
+    return horizontal_position * depth
+
+
+print(calculation_planned_course(data_lst_2d))
